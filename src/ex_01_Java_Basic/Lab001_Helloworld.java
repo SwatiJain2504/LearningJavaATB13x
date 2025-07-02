d620bddf7ae58c9cf2cfcf012e11cb875c3c8cc2
SwatiJain2504 <swati250490@gmail.com>
2025-07-02 10:54:00 +0530

    New changes Commit

--- src/ex_01_Java_Basic/Lab001_Helloworld.java
+++ src/ex_01_Java_Basic/Lab001_Helloworld.java
@@ -3,5 +3,6 @@ package ex_01_Java_Basic;
 public class Lab001_Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Swati here");
     }
 }
